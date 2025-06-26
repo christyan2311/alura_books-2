e7fab7da5fc56d0874f38bcf09e9d248b7a09bc0
Christyan Roberto Silva <christyanroberto31@gmail.com>
2025-06-26 12:21:25 -0300

    Fetch de Livros no Console

---
+++ main.js
@@ -0,0 +1,10 @@
+let Livros = []
+const endpointDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'
+getBuscarLivrosDaAPI()
+
+async function getBuscarLivrosDaAPI() {
+    const res = await fetch(endpointDaAPI)
+    Livros = await res.json()
+    console.table(Livros)
+
+}
