8017b3616b1a8e57ecfe06416c8fa7e7addfd7d7
Christyan Roberto Silva <christyanroberto31@gmail.com>
2025-06-30 21:41:19 -0300

    Exibir livros filtrados na tela

--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -1,6 +1,7 @@
 const elementoParaInserirLivros = document.getElementById('livros')
 
 function exibirOsLivrosNaTela(listaDeLivros) {
+    elementoParaInserirLivros.innerHTML = '' // Limpa o conteúdo anterior
     listaDeLivros.forEach(livro => {
         elementoParaInserirLivros.innerHTML += `
         <div class="livro">
