df0757813c309eb5c746a67d69d7a37ae57f191b
Christyan Roberto Silva <christyanroberto31@gmail.com>
2025-06-30 21:37:43 -0300

    Filter de Livros

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -4,5 +4,5 @@ botoes.forEach(btn =>  btn.addEventListener('click', filtrarLivros));
 function filtrarLivros() {
     const elementoBtn = document.getElementById(this.id);
     const categoria = elementoBtn.value;
-    let livrosFiltrados = livros.filter(livro => livro.categoria === categoria);    
+    let livrosFiltrados = livros.filter(livro => livro.categoria === categoria);   
 }
